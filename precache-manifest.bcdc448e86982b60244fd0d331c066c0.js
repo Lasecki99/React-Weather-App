0d928ea997c51268e2c96250530639815d635bd7
Marcin Lasecki <marcin.lasecki99@gmail.com>
2019-04-19 15:21:39 +0200

    Updates

---
+++ precache-manifest.bcdc448e86982b60244fd0d331c066c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bec730ca5bb383bc2c31",
+    "url": "/React-Weather-App/static/js/runtime~main.29983489.js"
+  },
+  {
+    "revision": "2c9c3317d1814cbf83ff",
+    "url": "/React-Weather-App/static/js/main.38fc76ef.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/React-Weather-App/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2c9c3317d1814cbf83ff",
+    "url": "/React-Weather-App/static/css/main.942d4869.chunk.css"
+  },
+  {
+    "revision": "1a051cf774557aa25210329be012c4e3",
+    "url": "/React-Weather-App/index.html"
+  }
+];
