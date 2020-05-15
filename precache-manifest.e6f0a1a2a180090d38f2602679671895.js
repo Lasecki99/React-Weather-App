c7a23bec2a9ff0b11afbf7ef4ef940777abef147
Marcin Lasecki <marcin.lasecki@pearson.com>
2020-05-15 17:18:00 +0200

    Updates

---
+++ precache-manifest.e6f0a1a2a180090d38f2602679671895.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0554e13c15889e2338a0e6b8e9c53b3",
+    "url": "/React-Weather-App/index.html"
+  },
+  {
+    "revision": "318477e496bce9b2f9a4",
+    "url": "/React-Weather-App/static/css/main.6a3cdd7c.chunk.css"
+  },
+  {
+    "revision": "6bf45c924189087f2402",
+    "url": "/React-Weather-App/static/js/2.c4582122.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Weather-App/static/js/2.c4582122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "318477e496bce9b2f9a4",
+    "url": "/React-Weather-App/static/js/main.a27eaad3.chunk.js"
+  },
+  {
+    "revision": "5fffbbeca58ad44ee78b",
+    "url": "/React-Weather-App/static/js/runtime-main.2ce69d55.js"
+  },
+  {
+    "revision": "4e72ab63e8e13a85d3c837c842bc3b7d",
+    "url": "/React-Weather-App/static/media/fog-1535201_1920.4e72ab63.jpg"
+  }
+]);
